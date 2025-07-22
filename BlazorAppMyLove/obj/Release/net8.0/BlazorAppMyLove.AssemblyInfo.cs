f99eb3b10c0c7bbf2ed261ca0e8338075bacb3ab
H1PEZECA <josemendess004@gmail.com>
2025-07-22 16:51:35 -0300

    fix any problems

--- BlazorAppMyLove/obj/Release/net8.0/BlazorAppMyLove.AssemblyInfo.cs
+++ BlazorAppMyLove/obj/Release/net8.0/BlazorAppMyLove.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppMyLove")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa21a987f0ff2e9750b011b0210584fff9c512cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppMyLove")]
