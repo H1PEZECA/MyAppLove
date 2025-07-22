78f78bcdde4a23a15659025f6eb86ca9e6e89d3d
H1PEZECA <josemendess004@gmail.com>
2025-07-22 17:00:49 -0300

    chore: added read.me

--- BlazorAppMyLove/obj/Release/net8.0/BlazorAppMyLove.AssemblyInfo.cs
+++ BlazorAppMyLove/obj/Release/net8.0/BlazorAppMyLove.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppMyLove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa21a987f0ff2e9750b011b0210584fff9c512cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc2241b61c3501b39f603ecb7e3d3502024ad3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppMyLove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppMyLove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
